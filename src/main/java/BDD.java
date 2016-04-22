290e048dde81e69bb7e733a4eae06393ca967bbf
Cory <franksco39@gmail.com>
2016-04-22 11:32:25 -0700

    add basic layout of project

---
+++ src/main/java/BDD.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.lang.Object;
+import java.lang.String;
+import spark.ModelAndView;
+import static java.lang.System.out;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import java.util.Random;
+
+public class BDD {
+
+
+  return sentence
+}
---
+++ src/test/java/BDDTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class BDD {
+
+  @Test
+  public void replacement_TestForA() {
+    BDD youApp = new BDD();
+    expected.add(1);
+    assertEquals(expected, yourApp.replaceLetter(""));
+  }
+
+
+}
